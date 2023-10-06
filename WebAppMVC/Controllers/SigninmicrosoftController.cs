18806796c16e28c9c4a4675bee8cc15cf336d4f4
Anton Mishin <ton4ik07@yandex.ru>
2023-10-07 02:14:29 +0400

    Добавил проект WebAppMVC

---
+++ WebAppMVC/Controllers/SigninmicrosoftController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SigninmicrosoftController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Home/Index");
+        }
+    }
+}
