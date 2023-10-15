228b311d6009046200a1b54d8bd01ed6bbb2f712
Anton Mishin <ton4ik07@yandex.ru>
2023-10-15 13:55:20 +0400

    Убрали не нужные страницы

--- WebApp/Pages/Profiles/CurrentUserProfile.cshtml.cs
+++ WebApp/Pages/Profiles/CurrentUserProfile.cshtml.cs
@@ -1,43 +1,50 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages.Profiles
 {
+    
     public class CurrentUserProfileModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public CurrentUserProfileModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public async Task OnGet()
         {
-            var asd = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/identity/claims/identityprovider")?.Value;
+            //Добавить логику получение данных профиля из БД:ProfileUser
 
-            //signInManager.
+            var asdfdf = await _signInManager.GetExternalAuthenticationSchemesAsync();
+            var b = await _signInManager.ExternalLoginSignInAsync("Microsoft", "0f29ee4d4b9131de", true);
 
-            var props = new AuthenticationProperties();
-            //props.StoreTokens(info.AuthenticationTokens);
-            //props.IsPersistent = false;
+            //var authenticateResult = await HttpContext.AuthenticateAsync("Microsoft");
+            //var identity = HttpContext.User.Identity;
+            //var identities = HttpContext.User.Identities;
+            //;
 
-            var authenticateResult = await HttpContext.AuthenticateAsync("Microsoft");
+            //if (authenticateResult.Succeeded)
+            //{
+            //    // Получите `authorization_code` из authenticateResult.Properties
+            //    IDictionary<string, string?> items = authenticateResult.Properties.Items;
 
-            if (authenticateResult.Succeeded)
-            {
-                // Получите `authorization_code` из authenticateResult.Properties
-                IDictionary<string, string?> items = authenticateResult.Properties.Items;
+            //    // Теперь вы можете использовать `authorizationCode` для запроса токена OAuth2.
+            //    // Дополнительная логика обработки токена здесь.
 
-                // Теперь вы можете использовать `authorizationCode` для запроса токена OAuth2.
-                // Дополнительная логика обработки токена здесь.
+            //    var accessToken = authenticateResult.Properties.GetTokenValue("access_token");
+            //    var refreshToken = authenticateResult.Properties.GetTokenValue("refresh_token");
 
-                var accessToken = authenticateResult.Properties.GetTokenValue("access_token");
-                var refreshToken = authenticateResult.Properties.GetTokenValue("refresh_token");
+            //    // Получите информацию о пользователе
+            //    var user = authenticateResult.Principal;
+            //}
+            //else
+            //{
+            //    // Обработка неудачной аутентификации
+            //}
 
-                // Получите информацию о пользователе
-                var user = authenticateResult.Principal;
-            }
-            else
-            {
-                // Обработка неудачной аутентификации
-            }
-            
         }
     }
 }
