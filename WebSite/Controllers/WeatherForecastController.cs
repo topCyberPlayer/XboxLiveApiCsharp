c8d00713a94dc8729b8d5257359a45c3facd0889
Anton Mishin <ton4ik07@yandex.ru>
2024-01-18 19:04:55 +0400

    Добавил приложение WebSite

---
+++ WebSite/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5256/api");
+        private readonly HttpClient _client;
+        public WeatherForecastController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<WeatherForecastViewModel>? weatherList = new List<WeatherForecastViewModel>();
+            HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + "/Weatherforecast/Get");
+
+            if (response.IsSuccessStatusCode)
+            {
+                weatherList = await response.Content.ReadFromJsonAsync<List<WeatherForecastViewModel>>();
+            }
+
+            return View(weatherList);
+        }
+    }
+}
