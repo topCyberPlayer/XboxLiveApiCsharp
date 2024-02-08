9b013ae737bf45390f46caa0f16ca3b3ea177fb1
Anton Mishin <ton4ik07@yandex.ru>
2024-02-08 19:33:32 +0400

    Добавил новые controller

---
+++ XboxLiveService/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XboxLiveService.Controllers
+{
+    public class GameController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ XboxLiveService/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XboxLiveService.Controllers
+{
+    public class ProfileController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
