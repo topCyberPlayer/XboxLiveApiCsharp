1058e4354cdcef54f53c81939b408d07a9fa1f35
Anton Mishin <ton4ik07@yandex.ru>
2024-04-24 23:44:16 +0400

    Добавил RazorPage Gamer

---
+++ WebApp/Pages/Gamer/Games.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Gamer
+{
+    public class GamesModel : PageModel
+    {
+        public void OnGet(string gamertag)
+        {
+        }
+    }
+}
