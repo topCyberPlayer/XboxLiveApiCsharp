4738c4bb560e61de25c571e6a16a9fa816bbc676
Anton Mishin <ton4ik07@yandex.ru>
2024-05-23 19:29:26 +0400

    Разрабатываю заполнение БД

---
+++ XblApp.Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XblApp.Domain.Entities;
+
+namespace XblApp.Infrastructure.Data
+{
+    public static class DatabaseStartupHelpers
+    {
+        public static async Task<WebApplication> SetupDatabaseAsync(this WebApplication app)
+        {
+            // Initialize the database with seed data
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IWebHostEnvironment>();
+                var context = scope.ServiceProvider.GetRequiredService<XblAppDbContext>();
+
+                try
+                {
+                    var arePendingMigrations = context.Database.GetPendingMigrations().Any();
+                    await context.Database.MigrateAsync();
+                    await context.SeedDatabaseIfNoGamersAsync(env.WebRootPath);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+            }
+
+            return app;
+        }
+    }
+
+    public static class SetupHelpers
+    {
+        private const string SeedDataSearchName = "Apress books*.json";
+        public const string SeedFileSubDirectory = "seedData";
+
+        public static async Task<int> SeedDatabaseIfNoGamersAsync(this XblAppDbContext context, string dataDirectory)
+        {
+            var numGamers = context.Gamers.Count();
+            if (numGamers == 0)
+            {
+                var gamers = GamerJsonLoader.LoadGamers(Path.Combine(dataDirectory, SeedFileSubDirectory), SeedDataSearchName).ToList();
+                await context.Gamers.AddRangeAsync(gamers);
+                await context.SaveChangesAsync();
+            }
+            return numGamers;
+        }
+    }
+
+    public static class GamerJsonLoader
+    {
+        // Add initial gamers
+        private static readonly Gamer[] _gamers =
+        [
+            new Gamer { GamerId = 1, Gamertag = "Gamer1", Gamerscore = 1000, Bio = "Bio1", Location = "Location1" },
+            new Gamer { GamerId = 2, Gamertag = "Gamer2", Gamerscore = 2000, Bio = "Bio2", Location = "Location2" }
+        ];
+
+        // Add initial games
+        private static readonly Game[] _games =
+        {
+            new Game { GameId = 1, GameName = "Game1", TotalAchievements = 50, TotalGamerscore = 1000 },
+            new Game { GameId = 2, GameName = "Game2", TotalAchievements = 100, TotalGamerscore = 2000 }
+        };
+
+
+        // Add initial gamer-game links
+        private static readonly GamerGame[] _gamerGame = 
+        [
+            new GamerGame { GamerId = 1, GameId = 1, CurrentAchievements = 30, CurrentGamerscore = 600 },
+            new GamerGame { GamerId = 1, GameId = 2, CurrentAchievements = 50, CurrentGamerscore = 1000 },
+            new GamerGame { GamerId = 2, GameId = 1, CurrentAchievements = 50, CurrentGamerscore = 1000 }
+        ];
+
+        public static IEnumerable<Gamer> LoadGamers(string fileDir, string fileSearchString)
+        {
+
+        }
+    }
+}
--- XblApp/Program.cs
+++ XblApp/Program.cs
@@ -12,10 +12,53 @@ namespace XblApp
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-            string? dbProvider = builder.Configuration.GetConnectionString("DatabaseProvider");
-            builder.Services.RegisterApplicationServices(dbProvider);
-            var app = builder.Build();
+            builder.RegisterApplicationServices();
+            WebApplication? app = builder.Build();
 
+            app.ConfigureMiddleware();
+            app.RegisterEndpoints();
+
+            app.Run();
+        }
+    }
+
+    public static partial class ServiceInitializer
+    {
+        public static WebApplicationBuilder RegisterApplicationServices(this WebApplicationBuilder builder)
+        {
+            string? dbProvider = 
+            builder.Configuration.GetConnectionString("DatabaseProvider");
+
+            builder.Services.AddScoped<IXboxLiveService, XboxLiveService>();
+            builder.Services.AddScoped<IGamerRepository, GamerRepository>();
+            builder.Services.AddScoped<GamerProfileUseCase>();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddHttpClient();
+            builder.Services.AddRazorPages();
+
+
+            switch (dbProvider)
+            {
+                case "MsSql":
+                    builder.Services.AddDbContext<XblAppDbContext, MsSqlDbContext>();
+                    break;
+                case "PostgreSql":
+                    builder.Services.AddDbContext<XblAppDbContext, PostgresDbContext>();
+                    break;
+            }
+
+            return builder;
+
+            //builder.Services.AddDbContext<XblAppDbContext>(options => options.UseSqlServer(connectionString));
+            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+
+    public static partial class MiddlewareInitializer
+    {
+        public static WebApplication ConfigureMiddleware(this WebApplication app)
+        {
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -32,43 +75,21 @@ namespace XblApp
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.SetupDatabaseAsync();
 
             //app.UseAuthorization();
 
-            app.MapRazorPages();
-
-            app.Run();
+            return app;
         }
     }
 
-    public static partial class ServiceInitializer
+    public static partial class EndpointMapper
     {
-        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services, string dbProvider)
+        public static WebApplication RegisterEndpoints(this WebApplication app)
         {
-            services.AddScoped<IXboxLiveService, XboxLiveService>();
-            services.AddScoped<IGamerRepository, GamerRepository>();
-            services.AddScoped<GamerProfileUseCase>();
+            app.MapRazorPages();
 
-            services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddHttpClient();
-            services.AddRazorPages();
-
-
-            switch (dbProvider)
-            {
-                case "MsSql":
-                    services.AddDbContext<XblAppDbContext, MsSqlDbContext>();
-                    break;
-                case "PostgreSql":
-                    services.AddDbContext<XblAppDbContext, PostgresDbContext>();
-                    break;
-            }
-
-            return services;
-
-            //services.AddDbContext<XblAppDbContext>(options => options.UseSqlServer(connectionString));
-            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            return app;
         }
     }
 }
