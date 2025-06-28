d8c028aef91fe2aad2ec5ae52bd1c277097fab09
Anton Mishin <kgbtoplayer@gmail.com>
2025-06-28 13:25:41 +0400

    отключил https Redirect

--- XblApp.UI/Program.cs
+++ XblApp.UI/Program.cs
@@ -42,7 +42,7 @@ namespace XblApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
