d21d23e725bc5ff06b3e2ccda3ceaf68582bb33a
Anton Mishin <kgbtoplayer@gmail.com>
2025-08-29 19:16:32 +0500

    Написал тест кейс для GameController POST

--- XblApp.API.Test/Controllers/GameControllerTest.cs
+++ XblApp.API.Test/Controllers/GameControllerTest.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Net;
-using System.Text.Json;
-using Domain.DTO;
+﻿using Domain.DTO;
+using Domain.Requests;
+using Humanizer;
 using Infrastructure.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace XblApp.API.Test.Controllers
 {
@@ -68,6 +71,66 @@ namespace XblApp.API.Test.Controllers
 
                 // Проверяем отдельные свойства (пример)
                 Assert.True(game.GameId > 0);
+                Assert.False(string.IsNullOrWhiteSpace(game.GameName));
+            }
+        }
+
+        [Fact]
+        public async Task POST_ShouldOk()
+        {
+            using (var scope = factory.Services.CreateScope())
+            {
+                // Arrange
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                GameRequest game = new()
+                {
+                    GameName = "Test Game",
+                    TotalAchievements = 10,
+                    TotalGamerscore = 100,
+                    ReleaseDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                    Description = "This is a test game.",
+                    Achievements = new List<AchievementRequest>
+                    {
+                        new AchievementRequest
+                        {
+                            Name = "First Achievement",
+                            Description = "Unlock the first achievement.",
+                            Gamerscore = 10,
+                            IsSecret = false
+                        },
+                        new AchievementRequest
+                        {
+                            Name = "Secret Achievement",
+                            Description = "Unlock the secret achievement.",
+                            Gamerscore = 20,
+                            IsSecret = true
+                        }
+                    }
+                };
+
+                string? jsonSer = JsonSerializer.Serialize(game);
+                StringContent? content = new StringContent(jsonSer, Encoding.UTF8, "application/json");
+
+                // Act
+                HttpResponseMessage? response = await _client.PostAsync($"api/game", content);
+
+                // Assert: статус ответа
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+                // Assert: контент
+                string jsonDesiar = await response.Content.ReadAsStringAsync();
+                Assert.False(string.IsNullOrWhiteSpace(jsonDesiar));
+
+                GameDTO? gameResp = JsonSerializer.Deserialize<GameDTO>(jsonDesiar, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                Assert.NotNull(gameResp);
+
+                // Проверяем что вернулся именно GameDTO
+                Assert.IsType<GameDTO>(gameResp);
+
+                // Проверяем отдельные свойства (пример)
+                Assert.True(gameResp.GameId > 0);
                 Assert.False(string.IsNullOrWhiteSpace(game.GameName));
             }
         }
