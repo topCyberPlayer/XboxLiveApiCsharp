93909c80dfa4ee4264feedac898c921f7f05fdee
Anton Mishin <kgbtoplayer@gmail.com>
2025-09-02 20:48:42 +0500

    Создал миграцию: update_GamerConfiguration

---
+++ Infrastructure/Migrations/20250902154729_update_GamerConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_GamerConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gamers_AspNetUsers_UserId",
+                table: "Gamers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Gamers_UserId",
+                table: "Gamers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Gamers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Gamers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gamers_UserId",
+                table: "Gamers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gamers_AspNetUsers_UserId",
+                table: "Gamers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
